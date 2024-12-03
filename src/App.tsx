e31bb65e5e80eb4a201e1ffa4ad8b9f3c82f0f3b
pritzza <jonathanzakharov@gmail.com>
2024-12-03 12:44:09 -0800

    spectral DB feature added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import AppLayout from "./components/AppLayout";
+import { AppContextProvider } from "./components/AppLayout";  // Import the context provider
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <AppLayout></AppLayout>
+      <AppContextProvider>
+        <AppLayout />
+      </AppContextProvider>
     </MantineProvider>
   );
 }
